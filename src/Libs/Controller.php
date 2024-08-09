3b6697b9f4dddbec39f799fbc665d2a45a23e7e0
rod773 <rodrigoarcadio1@gmail.com>
2024-08-09 13:13:08 +0200

    update

--- src/Libs/Controller.php
+++ src/Libs/Controller.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App\Libs;
+namespace  Libs;
 
 class Controller{
 
     public function response(array $data, int $code=200){
+
+         http_response_code($code);
+
+         echo json_encode($data);
+
+         exit;
+
+    }
+
+    public function exists(array $data, array $parameters){
+
+        $missings = array_diff($parameters,array_keys($data));
+
+        if(!empty($missing)){
+
+            $string = implode(",",$missing);
+
+            $this->response([
+                "message" => "missing parameters : $string"
+            ]);
+        }
 
     }
 

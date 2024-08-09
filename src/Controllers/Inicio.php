ff8ef69c40dfc8b920341eb434cd569d470a007f
rod773 <rodrigoarcadio1@gmail.com>
2024-08-09 16:42:39 +0200

    update

--- src/Controllers/Inicio.php
+++ src/Controllers/Inicio.php
@@ -6,9 +6,12 @@ namespace Controllers;
 use Libs\Controller;
 
 
+
 class Inicio extends Controller{
 
     public function __construct($data){
+
+        parent::__construct();
 
         $this->exists(['email','phone'],$data);
 

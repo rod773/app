454ab386f10e3bdfee6e744b35bc3ef0a19679fd
rod773 <rodrigoarcadio1@gmail.com>
2024-08-09 12:54:06 +0200

    update

--- src/Libs/Controller.php
+++ src/Libs/Controller.php
@@ -1,10 +1,29 @@
 <?php
 
-namespace App\Libs;
+namespace  Libs;
 
 class Controller{
 
     public function response(array $data, int $code=200){
+
+        http_response_code($code);
+
+        echo json_encode($data);
+
+        exit;
+
+    }
+
+
+    public function exists(array $data, array $parameters){
+
+
+        $missing = array_diff($parameters,array_keys($data));
+
+        if($missing){
+            
+        }
+
 
     }
 

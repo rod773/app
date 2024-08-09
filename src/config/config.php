3e70a90c0762aa9ba46b38757e15bec4213a312d
rod773 <rodrigoarcadio1@gmail.com>
2024-08-09 09:58:29 +0200

    update

---
+++ src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+
+define ("HOST","localhost");
+define ("DB_NAME","wordpress1");
+define ("DB_USER","root");
+define ("DB_PASS","");
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4e8c9c47822345a308242142edb358d7c36f71f7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4e8c9c47822345a308242142edb358d7c36f71f7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
